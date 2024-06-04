0892996fa0b0d6d32b19b114716b0c3e183be6e4
1nazar1l <116957441+1nazar1l@users.noreply.github.com>
2024-06-04 19:28:22 +0300

    update files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import Collection from './components/Collection';
+import Pagination from './components/Pagination';
+import './index.css';
+import data from "./data/data.json"
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [collection,setCollections] = useState(data)
+  const [searchValue,setSearchValue] = useState("")
+  const [categoryId,setCategoryId] = useState(0)
+  const [currentPage, setCurrentPage] = useState(1)
+  const itemsPerPage = 3
+  let numPages = Math.ceil(collection.collections.filter(
+    (item) => {
+      return (
+        (categoryId === 0 || categoryId === item.category) &&
+        (searchValue ? item.name.toLowerCase().includes(searchValue.toLowerCase()) : true)
+      )
+    }
+  ).length/itemsPerPage)
+return (
+  <div className='App'>
+    <h1>Моя коллекция фотографий</h1>
+    <div className='top'>
+      <ul className='tags'>
+        {collection.categories.map((item, index) => (
+          <li onClick={() => {
+            setCategoryId(index)
+            if(item.name !== "Все"){
+              return setCurrentPage(1)
+            }
+          }} className={index === categoryId ? "active" : ""} key={index}>{item.name}</li>
+        ))}
+      </ul>
+      <input placeholder='Поиск по названию' className='search-input' value={searchValue} onChange={(event) => setSearchValue(event.target.value)}></input>
     </div>
-  );
+    <div className='content'>
+      {collection.collections.filter((item) => {
+        return (
+          (categoryId === 0 || categoryId === item.category) && 
+          (searchValue ? item.name.toLowerCase().includes(searchValue.toLowerCase()) : true)
+        )
+      })
+      .slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage)
+      .map((item) => (
+        <Collection name={item.name} images={item.photos}/>
+      ))}
+    </div>
+    {numPages && <Pagination currentPage={currentPage} setCurrentPage={setCurrentPage} numPages={numPages}/>}
+  </div>
+)
 }
 
 export default App;
